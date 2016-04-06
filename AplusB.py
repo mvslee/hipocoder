05b2ca3b717b5395bb330f4955a0977f105f3870
Mavis Li <mvslee@gmail.com>
2016-04-06 15:18:46 +0800

    modify shortcoming

--- AplusB.py
+++ AplusB.py
@@ -6,10 +6,10 @@
 calculate the sum of each set'''
 
 while True:
-    try:
-        (x,y) = (int(x) for x in raw_input().split())
-        print x+y
-    except EOFError:
-        break
+	try:
+		(x,y) = (int(x) for x in raw_input().split())
+		print x+y
+	except EOFError:
+		break
 
 
